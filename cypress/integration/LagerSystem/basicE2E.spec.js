5a394c9821d6823e05ad12ac5219051fbba6d9b4
Andreas Zachrisson <Azachris@volvocars.com>
2021-01-16 22:15:59 +0100

    Adds docker-compose and e2e tests

---
+++ cypress/integration/LagerSystem/basicE2E.spec.js
@@ -0,0 +1,38 @@
+describe('basicFuntionality', () => {
+    const PIZZA_ORDER_BASE_URL ="http://localhost:7500/";
+    const STORAGE_API_BASE_URL = "http://localhost:7501/";
+    let currentIndex;
+
+    it('Should load the StorageApi mainpage', () => {
+        cy.visit(STORAGE_API_BASE_URL);
+        cy.url().should('eq', STORAGE_API_BASE_URL);
+    });
+    it('Should add 1 "Ost" to the storage', () => {
+        cy.get("#Add-Ost-amount").type(1);
+        cy.get('#Add-Ost-submit').click();
+        cy.get('#Ost-current-amount').contains('1')
+    });
+    it('Should add 1 "Tomatsås" to the storage', () => {
+        cy.get("#Add-Tomatsås-amount").type(1);
+        cy.get('#Add-Tomatsås-submit').click();
+        cy.get('#Tomatsås-current-amount').contains('1')
+    });
+    it('Should create a pizza order through Ajax successfully', () => {
+        cy.request('POST', PIZZA_ORDER_BASE_URL + 'Orders', [ "Margarita" ])
+        .then((response) => {
+          expect(response.status).to.eq(200)
+          currentIndex = response.body.id
+        });
+    });
+    it('Should confirm a pizza order through Ajax successfully', () => {
+        cy.request({
+            method: 'PUT',
+            url: PIZZA_ORDER_BASE_URL + 'Confirm?orderId=' + currentIndex,
+        }).then((response) => {
+          expect(response.status).to.eq(200)
+        })
+    });
+    it('Should refresh the storageApi homepage to show current storage values', () => {
+        cy.reload();
+    });
+});
