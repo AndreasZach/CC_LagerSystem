1934bdfcecda1f417c1a657bc9152625ad3e3dcb
Andreas Zachrisson <Azachris@volvocars.com>
2021-01-18 17:43:20 +0100

    Fixes database being seeded even though it already contained data

--- StorageApi/Startup.cs
+++ StorageApi/Startup.cs
@@ -61,28 +61,31 @@ namespace StorageApi
                     .GetService<StorageItemContext>()
                     ?.Database
                     .Migrate();
-                serviceScope.ServiceProvider
-                    .GetService<StorageItemContext>()
-                    ?.AddRange(new List<StorageItem>
+                if (!serviceScope.ServiceProvider.GetService<StorageItemContext>().StorageItems.Any())
                 {
-                    new StorageItem("Ost"){ ItemAmount = 0},
-                    new StorageItem("Tomatsås"){ ItemAmount = 0},
-                    new StorageItem("Skinka"){ ItemAmount = 0},
-                    new StorageItem("Ananas"){ ItemAmount = 0},
-                    new StorageItem("Kebab"){ ItemAmount = 0},
-                    new StorageItem("Champinjoner"){ ItemAmount = 0},
-                    new StorageItem("Lök"){ ItemAmount = 0},
-                    new StorageItem("Feferoni"){ ItemAmount = 0},
-                    new StorageItem("Isbergssallad"){ ItemAmount = 0},
-                    new StorageItem("Tomat"){ ItemAmount = 0},
-                    new StorageItem("Kebabsås"){ ItemAmount = 0},
-                    new StorageItem("Räkor"){ ItemAmount = 0},
-                    new StorageItem("Musslor"){ ItemAmount = 0},
-                    new StorageItem("Kronärtskocka"){ ItemAmount = 0},
-                    new StorageItem("Coca cola"){ ItemAmount = 0},
-                    new StorageItem("Fanta"){ ItemAmount = 0},
-                    new StorageItem("Sprite"){ ItemAmount = 0}
-                });
+                    serviceScope.ServiceProvider
+                        .GetService<StorageItemContext>()
+                        ?.AddRange(new List<StorageItem>
+                        {
+                            new StorageItem("Ost") {ItemAmount = 0},
+                            new StorageItem("Tomatsås") {ItemAmount = 0},
+                            new StorageItem("Skinka") {ItemAmount = 0},
+                            new StorageItem("Ananas") {ItemAmount = 0},
+                            new StorageItem("Kebab") {ItemAmount = 0},
+                            new StorageItem("Champinjoner") {ItemAmount = 0},
+                            new StorageItem("Lök") {ItemAmount = 0},
+                            new StorageItem("Feferoni") {ItemAmount = 0},
+                            new StorageItem("Isbergssallad") {ItemAmount = 0},
+                            new StorageItem("Tomat") {ItemAmount = 0},
+                            new StorageItem("Kebabsås") {ItemAmount = 0},
+                            new StorageItem("Räkor") {ItemAmount = 0},
+                            new StorageItem("Musslor") {ItemAmount = 0},
+                            new StorageItem("Kronärtskocka") {ItemAmount = 0},
+                            new StorageItem("Coca cola") {ItemAmount = 0},
+                            new StorageItem("Fanta") {ItemAmount = 0},
+                            new StorageItem("Sprite") {ItemAmount = 0}
+                        });
+                }
                 serviceScope.ServiceProvider
                     .GetService<StorageItemContext>()
                     ?.SaveChanges();
